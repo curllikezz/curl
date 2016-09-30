f64ef80687016e2e53b671ae5eab4fb42818d336
sxdsadsadas <trongthien135@gmail.com>
2016-09-30 20:39:49 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$domain= 'http://ungdung.online/a/'; // không có / ở cuối
-$linktoken= 'http://ungdung.online/a/token.txt'; // nên để trong file txt nhé
-$id_hoadz='100007232988005'; // ID User VIP
+$domain= 'https://github.com/curllikezz/a/'; // không có / ở cuối
+$linktoken= 'https://github.com/curllikezz/a/token.txt'; // nên để trong file txt nhé
+$id_hoadz='100010142492158'; // ID User VIP
 ?>
